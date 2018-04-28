17cc5d6d5acb2e2a73642dda2a1d01a6af819632
kevin <kbradner1122gmail.com>
2018-04-28 15:14:21 -0400

    restructured server as class, added functions

--- server.py
+++ server.py
@@ -1,9 +1,40 @@
 import socket
 
+import socket
 import json
+import sys
 
-latest_command = ""
+class HoldEmServer:
 
-def unpack_cmd(data):
-    latest_command = json.loads(data)
+    def __init__(self):
+        self.client_list = []
+        self.HOST = ''
+        self.PORT = 8888
 
+        try:
+            self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("socket created")
+        except socket.error:
+            print('Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1])
+            sys.exit();
+
+        try:
+            self.serversocket.bind((self.HOST, self.PORT))
+        except socket.error:
+            print("bind failed")
+
+    def acceptClients(self):
+        #listen for 2 players
+        self.serversocket.listen(2)
+
+        print("socket listening")
+
+    def collectCommand(stateMsg, options, self):
+        data = {"message" : stateMsg, "options" : options}
+
+    def unpack_data(data, self):
+        new_dict = json.loads(data)
+        return new_dict
+
+    def pack_dict(dict, self):
+        data = json.dumps(dict)
