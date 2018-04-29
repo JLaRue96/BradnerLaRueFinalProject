3b4e6bd767bd161ed4df88dab8ecedf866f72ac2
Jack LaRue <jacklarue7496@gmail.com>
2018-04-29 17:47:10 -0400

    New stuff (#8)
    Finalize play round code

--- server.py
+++ server.py
@@ -18,11 +18,14 @@ class HoldEmServer:
         self.client_list = []
         self.HOST = ''
         self.PORT = 8888
-        self.num_players = 1
+        self.num_players = 2
         self.addresses = {}
         self.sockets = {}
         self.conn = {}
         self.addr = {}
+
+        # Global flag to determine if somebody has folded.
+        self.has_folded = False
 
         # Global information regarding how much money is on the table.
         self.pot = 0
@@ -54,6 +57,7 @@ class HoldEmServer:
             self.sockets[i].listen(1)
             self.conn[i], self.addr[i] = self.sockets[i].accept()
             print("connection " + str(i + 1) + " of " + str(self.num_players) + " established.")
+            self.initialize_player(i)
 
     def collectCommand(self, stateMsg, options, playerNum):
         data = {"message" : stateMsg, "options" : options}
@@ -62,7 +66,7 @@ class HoldEmServer:
 
         dictIn = self.getResponse(playerNum)
         
-        print("client selection: " + dictIn["selection"])
+        # print("client selection: " + dictIn["selection"])
 
         return dictIn
 
@@ -112,14 +116,12 @@ class HoldEmServer:
         for the duration of the round.
         :return:
         """
-
         # Load and shuffle the deck
         deck = shuffle_deck(generate_deck())
 
         # Reset the table_card_list if it currently
         # contains cards.
         self.table_card_list = []
-
         # Gives two cards to each of the players.
         for player in self.player_list:
             first_card = remove_card_from_top(deck)
@@ -128,26 +130,15 @@ class HoldEmServer:
 
             player.set_hand(hand)
 
-        cards_on_table = self.get_table_list_string()
-
-        # Tells players what cards are on the table.
-        self.send_message_all(cards_on_table)
-
         # Sends information to the player about their current hands.
-        for i in range(self.num_players):
-            """
-            PSEUDO:
-            
-            -get player instance from i (pid)
-            -get hand from player
-            -formulate a string 
-            """
+        self.send_players_their_hands()
 
-        """
-        # TODO: for each client, send from server to
-        # client a string of each card name and rank.
+        # Handle first round of betting.
+        self.handle_betting()
 
-        # TODO: Each Client bets. Clients can also fold if need be.
+        # Checks to see if a player has folded.
+        if self.has_folded:
+            return
 
         # Play the flop cards.
         flop_ctr = 0
@@ -157,25 +148,62 @@ class HoldEmServer:
             self.table_card_list.append(card)
             flop_ctr += 1
 
-        # TODO: Send Clients list of cards. Should be printed to terminal.
-        # TODO: Each Client bets. Clients can also fold if need be.
+        cards_on_table = self.get_table_list_string()
+
+        # Tells players what cards are on the table.
+        self.send_message_all(cards_on_table)
+
+        # Sends information to the player about their current hands.
+        self.send_players_their_hands()
+
+        # Handle second round of betting.
+        self.handle_betting()
+
+        # Checks to see if a player has folded.
+        if self.has_folded:
+            return
 
         # Play the turn card.
         remove_card_from_top(deck)
         turn_card = remove_card_from_top(deck)
         self.table_card_list.append(turn_card)
 
-        # TODO: Send Clients list of cards. Should be printed to terminal.
-        # TODO: Each Client bets. Clients can also fold if need be.
+        cards_on_table = self.get_table_list_string()
+
+        # Tells players what cards are on the table.
+        self.send_message_all(cards_on_table)
+
+        # Sends information to the player about their current hands.
+        self.send_players_their_hands()
+
+        # Handle third round of betting.
+        self.handle_betting()
+
+        # Checks to see if a player has folded.
+        if self.has_folded:
+            return
 
         # Play the river card.
         remove_card_from_top(deck)
         river_card = remove_card_from_top(deck)
         self.table_card_list.append(river_card)
 
-        # TODO: Send Clients list of cards. Should be printed to terminal.
-        # TODO: Each Client bets. Clients can also fold if need be.
+        cards_on_table = self.get_table_list_string()
 
+        # Tells players what cards are on the table.
+        self.send_message_all(cards_on_table)
+
+        # Sends information to the player about their current hands.
+        self.send_players_their_hands()
+
+        # Handle last round of betting.
+        self.handle_betting()
+
+    def determine_winner(self):
+        """
+        Determines the winner and distributes the pot accordingly.
+        :return: Updated player statuses
+        """
         player_info_tuple_list = []
 
         # Get Player info tuples.
@@ -198,19 +226,90 @@ class HoldEmServer:
         # Get a list of winners from the player info tuples:
         winners = compare_hands(player_info_tuple_list)
 
-        # TODO: Send to clients the list of winners.
-        # TODO: Distribute pot values winners.
+        if len(winners) == 2:
+            self.send_message_all("You both won! Splitting the pot of size " + str(self.pot) + "evenly \n")
+            half_pot = int(self.pot/2)
+
+            for i in range(self.num_players):
+                player = self.get_player_from_id(i)
+
+                player.receive_winnings(half_pot)
+        else:
+            winner = winners[0]
+            winner_id = winner[0]
+
+            self.send_message_all(
+                "Player " + str(winner_id + 1) + " has won. They receive pot of size " + str(self.pot) + "\n",
+            )
+
+            player = self.get_player_from_id(winner_id)
+
+            player.receive_winnings(self.pot)
 
         self.reset_player_statuses()
-        
+        self.pot = 0
+        self.has_folded = False
+
+    def send_players_their_hands(self):
         """
+        Sends the player their current hand to be printed to terminal.
+        :return:
+        """
+        for i in range(self.num_players):
+
+            player_info = self.get_player_from_id(i)
+
+            hand = player_info.get_hand()
+
+            hand_str = '\nCards in your hand:\n'
+
+            for card in hand:
+                hand_str += card[0] + ' of ' + card[1] + '\n'
+
+            self.send_message(hand_str, i)
 
     def handle_betting(self):
         """
         players will now bet. During this time,
         players have the opportunity to bet, fold, raise, or call.
         """
-        pass
+
+        for i in range(self.num_players):
+            if not self.has_folded:
+
+                message = "Betting Phase: \n"
+                options = ["bet", "fold"]
+
+                player = self.get_player_from_id(i)
+
+                result = self.collectCommand(message, options, i)
+
+                if result["selection"] == "fold":
+                    player.fold()
+                    self.has_folded = True
+
+                    fold_msg = "You have folded"
+                    self.send_message(fold_msg, i)
+
+                elif result["selection"] == "bet":
+                    bet_amt = result["amount"]
+                    result_tup = player.place_bet(bet_amt)
+
+                    while not result_tup[0]:
+                        balance = player.get_earnings()
+
+                        fail_msg = "Your maximum balance is " + str(balance)
+                        fail_msg += ", \nplace a bet below this amount"
+                        result = self.collectCommand(fail_msg, options, i)
+                        bet_amt = result["amount"]
+                        result_tup = player.place_bet(bet_amt)
+
+                    # Pot is incremented by bet amount.
+                    self.pot += result_tup[1]
+                    bet_msg = "You have betted " + str(result_tup[1])
+                    bet_msg += "\nyour remaining currency is " + str(player.get_earnings())
+
+                    self.send_message(bet_msg, i)
 
     def get_table_list_string(self):
         """
@@ -264,6 +363,7 @@ players_present = True
 while players_present:
     myServer.collectCommand("Start a new round?", ["start", "quit"], 0)
     myServer.play_round()
+    myServer.determine_winner()
 
 myServer.collectCommand("test game state", ["bet", "fold"], 0)   
 time.sleep(5)
